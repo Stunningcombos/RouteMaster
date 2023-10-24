f5bd2c430c48dc56270b108c2a385992784259ca
Stunningcombos <133907009+Stunningcombos@users.noreply.github.com>
2023-10-24 03:23:29 +0000

    gb

--- app.js
+++ app.js
@@ -53,6 +53,11 @@ function setInputError(inputElement, message) {
     inputElement.parentElement.querySelector('.form__input-error-message').textContent = message;
 }
 
+function clearInputError(inputElement){
+    inputElement.classList.remove('form__input--error');
+    inputElement.parentElement.querySelector('.form__input-error-message').textContent = "";
+}
+
 document.addEventListener('DOMContentLoaded', () =>{
     const loginForm = document.querySelector('#login');
     const createAccountForm = document.querySelector('#createAccount');
@@ -83,6 +88,9 @@ document.addEventListener('DOMContentLoaded', () =>{
                 setInputError(inputElement, "Username must be at least 10 characters in length");
             }
         });
+        inputElement.addEventListener('input', e => {
+            clearInputError(inputElement);
+        })
     });
 });
 
